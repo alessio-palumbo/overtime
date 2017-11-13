5eb84df5c9ba1316d41931c8c2236aea0084d40c
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-11-13 15:33:23 +1100

    Add new button to navbar

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show]
+  before_action :set_post, only: [:show, :edit, :update]
 
   def index  
     @posts = Post.all
@@ -20,6 +20,17 @@ class PostsController < ApplicationController
       redirect_to @post, notice: 'Your post have been successfully created'
     else
       render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Your post have bee successfully updated'
+    else
+      render :edit
     end
   end
 
