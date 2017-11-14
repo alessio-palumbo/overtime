3dea531a684e2eb9f3495206695b3d375ae2c746
Alessio-Palumbo <alessio.palumbo4@gmail.com>
2017-11-14 11:00:34 +1100

    Remove registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'static#homepage'
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
